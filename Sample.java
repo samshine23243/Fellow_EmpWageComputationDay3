6280df621ff52999b422dc19b5f0ff6a1434a0a6
samshine <sameershinde23243@gmail.com>
2021-03-11 09:13:16 +0530

    [sameer] UC1 code added

--- Sample.java
+++ Sample.java
@@ -1,9 +1,24 @@
 
 public class Sample {
+	double Attendance()
+	{
+		double temp=Math.floor(Math.random()*10)%2;
+		return temp;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to employee wage computation....");
+		Sample obj=new Sample();
+		double attendance=obj.Attendance();
+		if(attendance==0)
+		{
+			System.out.println("Employee absent...");
+		}
+		else
+		{
+			System.out.println("employee present...");
+		}
 
 	}
 
